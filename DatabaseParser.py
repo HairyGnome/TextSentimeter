58e2d0b2733ae1fff0bb1b44463818d4dfcd670d
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2024-11-27 23:58:48 +0100

    Added cosnole output
    Added text tokenization, added console output with estimated time

--- DatabaseParser.py
+++ DatabaseParser.py
@@ -1,20 +1,50 @@
 import csv
+import math
 import os
+import sys
+import time
+
+from pyasn1_modules.rfc1902 import Integer
+
+from Tokenizer import Tokenizer
+
 
 class DatabaseParser:
+
+    def secToTime(self, sec):
+        days = math.floor(sec/86400)
+        sec = sec % 86400
+        hours = math.floor(sec/3600)
+        sec = sec % 3600
+        minutes = math.floor(sec/60)
+        sec = sec % 60
+        return days, hours, minutes, sec
 
     def parseData(self, path):
         data = []
         labels = []
+        tokenizer = Tokenizer()
         try:
             with open(path, 'r') as csvfile:
                 csvreader = csv.reader(csvfile)
-
-                fields = next(csvreader)
-
+                next(csvreader)
+                i = 0
+                start_time = time.time()
                 for row in csvreader:
-                    data.append(row[5])
+                    line_num = csvreader
+                    curr_time = time.time()
+                    time_elapsed = curr_time-start_time
+                    if i != 0:
+                        estimated_time = time_elapsed/i * (1600000-i)
+                    else:
+                        estimated_time = 99999999999
+                    estimated_time = self.secToTime(estimated_time)
+                    output = f'Row number: {i}/1,600,000\tTime elapsed: {math.floor(time_elapsed)} s\tEstimated time left: {estimated_time[0]} days {estimated_time[1]} hours {estimated_time[2]} minutes {round(estimated_time[3])} seconds'
+                    sys.stdout.write(f'\r{output}')
+                    sys.stdout.flush()
+                    data.append(tokenizer.tokenize(row[5]))
                     labels.append(row[0])
+                    i += 1
 
             return data, labels
 
