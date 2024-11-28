cc236c090e5bdd639d5ed3b3c4918f6b4eaf1ca7
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2024-11-28 15:13:30 +0100

    Fixed training

--- Sentimeter.py
+++ Sentimeter.py
@@ -1,7 +1,9 @@
 import os
 
+import numpy
 import tensorflow as tf
 from keras.src.metrics import MeanSquaredError
+from keras.src.utils import pad_sequences
 
 from DatabaseParser import DatabaseParser
 
@@ -19,16 +21,27 @@ def create_model():
         tf.keras.layers.Dense(units=40, activation='relu'),
         tf.keras.layers.Dense(units=20, activation='relu'),
         tf.keras.layers.Dense(units=10, activation='relu'),
-        tf.keras.layers.Dense(units=5, activation='softmax')
+        tf.keras.layers.Dense(units=1, activation='softmax')
     ])
     return model
 
 if __name__ == '__main__':
     model = create_model()
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError(), metrics=['accuracy', 'loss'])
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError(), metrics=['accuracy'])
 
     db_parser = DatabaseParser(10)
     data, labels, eval_data, eval_labels = db_parser.parse_data(os.curdir + '/data/dataset.csv')
+
+    print(data)
+
+    MAX_LENGTH = 100
+    data = pad_sequences(data, padding='post', maxlen=MAX_LENGTH)
+    eval_data = pad_sequences(eval_data, padding='post', maxlen=MAX_LENGTH)
+
+    data = numpy.array(data)
+    eval_data = numpy.array(eval_data)
+    labels = numpy.array(labels)
+    eval_labels = numpy.array(eval_labels)
 
     model.fit(data, labels, batch_size=batch_size, epochs=epochs, validation_split=0.1)
     model.evaluate(eval_data, eval_labels)
