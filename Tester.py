80f6ad6f04523d512adf24c4ab74aa79a718ac1d
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2024-11-28 16:17:27 +0100

    Added basic tests for parser

--- Tester.py
+++ Tester.py
@@ -1,8 +1,24 @@
+import os
+
 import pytest
 from Tokenizer import Tokenizer
+from DatabaseParser import DatabaseParser
 
 
-def testTokenizer():
+def test_tokenizer():
     tokenizer = Tokenizer()
     assert tokenizer.tokenize('Hello') == [72, 101, 108, 108, 111]
-    assert tokenizer.tokenize('Gaius Octavianus Germanicus Augustus') == [71, 97, 105, 1, 79, 99, 116, 97, 118, 105, 2, 1, 71, 101, 114, 109, 2, 105, 99, 1, 65, 117, 103, 0, 116, 0]
+    assert tokenizer.tokenize('Gaius Julius') == [71, 97, 1, 32, 74, 117, 108, 1]
+
+def test_database_parser():
+    parser = DatabaseParser(1, False)
+    tokenizer = Tokenizer()
+    data, labels, eval_data, eval_labels = parser.parse_data(os.curdir + '/data/dsm.csv')
+    assert data[0] == tokenizer.tokenize("@switchfoot http://twitpic.com/2y1zl - Awww, that's a bummer.  You shoulda got David Carr of Third Day to do it. ;D")
+    assert labels[0] == 0
+    assert data[6] == tokenizer.tokenize("Need a hug")
+    assert labels[6] == 0
+    assert eval_data == []
+    assert eval_labels == []
+    assert len(data) == 21
+    assert len(labels) == 21
