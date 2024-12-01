216a666e56f558941d55f12f8475c20c5bc700dd
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2024-12-01 13:52:50 +0100

    Added model saving, padding

--- Sentimeter.py
+++ Sentimeter.py
@@ -1,36 +1,36 @@
 import os
-
 import numpy
 import tensorflow as tf
-from keras.src.metrics import MeanSquaredError
-from keras.src.utils import pad_sequences
+from keras.utils import pad_sequences
 
 from DatabaseParser import DatabaseParser
 
-batch_size = 64
-epochs = 200
+batch_size = 256
+epochs = 5
 
 
 def create_model():
     model = tf.keras.models.Sequential([
         tf.keras.layers.InputLayer(input_shape=(None,)),
-        tf.keras.layers.Embedding(input_dim=1000, output_dim=64),
-        tf.keras.layers.GlobalAveragePooling1D(),
-        tf.keras.layers.Dense(units=120, activation='relu'),
-        tf.keras.layers.Dense(units=80, activation='relu'),
-        tf.keras.layers.Dense(units=40, activation='relu'),
-        tf.keras.layers.Dense(units=20, activation='relu'),
-        tf.keras.layers.Dense(units=10, activation='relu'),
-        tf.keras.layers.Dense(units=1, activation='softmax')
+        tf.keras.layers.Embedding(input_dim=256, output_dim=64),
+        tf.keras.layers.LSTM(units=64),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(units=64, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(units=32, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(units=16, activation='relu'),
+        tf.keras.layers.Dense(units=5, activation='softmax')
     ])
     return model
 
 if __name__ == '__main__':
     model = create_model()
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError(), metrics=['accuracy'])
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
     db_parser = DatabaseParser(10, True)
     data, labels, eval_data, eval_labels = db_parser.parse_data(os.curdir + '/data/dataset.csv')
+
 
     MAX_LENGTH = 100
     data = pad_sequences(data, padding='post', maxlen=MAX_LENGTH)
@@ -41,5 +41,13 @@ if __name__ == '__main__':
     labels = numpy.array(labels)
     eval_labels = numpy.array(eval_labels)
 
-    model.fit(data, labels, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-    model.evaluate(eval_data, eval_labels)
+    try:
+        model.fit(data, labels, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+        test_loss, test_accuracy = model.evaluate(eval_data, eval_labels)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        save_input = input('Save model? (Y/N) ')
+        save_input = save_input.capitalize()
+        if save_input == 'Y':
+            model.save(f'{os.curdir}/models/{test_accuracy}', overwrite=False)
